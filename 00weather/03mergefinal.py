95963a09f2a0d10fc40953d7573549b9e8da940f
notepi <panpengok@126.com>
2018-11-02 11:06:56 +0800

    update weather

--- 00weather/03mergefinal.py
+++ 00weather/03mergefinal.py
@@ -21,7 +21,8 @@ def file_name(file_dir):
 if __name__ == "__main__":
     rootfile="./mergedata"
     newpath="./mergefinal/"
-    
+    noth=pd.read_csv("./noth.csv")
+    nothtag=dict(zip(noth[u"方向"],noth["val"]))
     timelines=48
     #读跟目录下所有的目录
     dirl=[]
@@ -43,31 +44,39 @@ if __name__ == "__main__":
         except:
             pass
         #读取每个目录下的每个文件
+        finalFile=pd.DataFrame()
         for i in AllFile:
+            final=pd.DataFrame()
             #读取文件
             Data=pd.read_csv(i,encoding = "GBK")
+            Data["湿度"]=Data["湿度"]*100
+            Data[u"风向"]=Data[u"风向"].apply(lambda x: nothtag[x])
+
             #文件过短
             if len(Data) < timelines:
                 print(i)
                 continue
                 pass
-            final=pd.DataFrame()
+#            print(i,":",len(Data))
+#            final=pd.DataFrame()
             for j in range(timelines):
                 temp=Data.iloc[j:j-timelines,3:-1].reset_index(drop=True)
 #                print(j,"  ",j-timelines)
                 final=pd.concat([final,temp],axis=1)
 
 #                break
-                pass
+                pass#叠加循环
             temp=Data.iloc[j:j-timelines,:].reset_index(drop=True)
             final=pd.concat([temp.iloc[:,0:3],final,temp.iloc[:,-1]],axis=1)
-            
+            finalFile=pd.concat([finalFile,final])
 #            break
             pass#每个文件
-        del final["type"]
+
+            
+        del finalFile["type"]
         #新的文件名与原文件名一直，构造新的文件夹
         a=newpath+name.split('/')[-1]+"/"+name.split('/')[-1]+'.csv'
-        final.to_csv(a,encoding='GBK',index=False)
+        finalFile.to_csv(a,encoding='GBK',index=False)
 #        break
         pass#每个目录
     pass
